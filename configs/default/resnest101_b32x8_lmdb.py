8293d36185160f01c8a2fd00731377236c8f4ee8
huiyang865 <huiyang865@hotmail.com>
2020-11-13 17:34:13 +0800

    add training config

--- configs/default/resnest101_b32x8_lmdb.py
+++ configs/default/resnest101_b32x8_lmdb.py
@@ -1,4 +1,5 @@
 _base_ = [
-    '../_base_/models/resnest101.py', './imagenet_bs32_lmdb_balance.py',
-    './imagenet_bs256.py', './default_runtime.py'
+    '../_base_/models/resnest101_p.py',
+    '../_base_/datasets/imagenet_bs32_lmdb_balance_batch40.py',
+    '../_base_/schedules/imagenet_bs256_10e.py', '../_base_/default_runtime.py'
 ]
